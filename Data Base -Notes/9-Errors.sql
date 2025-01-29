de9f248e6fc2e25c3daab4c3df0dde4d1895ba3a
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-01-29 18:55:19 +0200

    Add files via upload

---
+++ Data Base -Notes/9-Errors.sql
@@ -0,0 +1,17 @@
+-- You should visit this link, where you will find many simple and large application projects on the database.
+-- https://github.com/abdelwahab-ahmed-shandy/Database-Design-with-ERD-EERD-Relational-Schemas-SQL-Implementation
+
+--Created By Abdelwahab Shandy
+--Don't Forget To Pray For Me And My Parents
+
+--Error 
+--===========================
+From  =>> Where  =>> Select  =>> Order By =>> OFFSET =>> FETCH NEXT 10 Rows Only
+
+
+--===========================
+--Cannot execute as the database principal because the principal "dbo" does not exist, this type of principal cannot be impersonated,or you do not have permission. (Framework Microsoft SqlClient Data Provider) 
+
+use [YourDatabaseName] EXEC sp_changedbowner 'sa' 
+
+--===========================
