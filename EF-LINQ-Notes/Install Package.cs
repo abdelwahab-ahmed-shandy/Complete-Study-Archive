d928b1294fff50d975c280e9f86f1b9b986e1b73
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-01-31 15:53:19 +0200

    Update Install Package.cs

--- EF-LINQ-Notes/Install Package.cs
+++ EF-LINQ-Notes/Install Package.cs
@@ -1,34 +1,44 @@
-- Open Package Manager Console :  
+// Open Package Manager Console :  
 
-EntityFrameworkCore
-لإدارة قواعد البيانات باستخدام نهج الـ ORM (Object-Relational Mapping) :
+// EntityFrameworkCore
+// لإدارة قواعد البيانات باستخدام نهج الـ ORM (Object-Relational Mapping) :
 
 Install-Package Microsoft.EntityFrameworkCore -Version 8.0.12
 
-EntityFrameworkCore.SqlServer
-إذا كنت تستخدم SQL Server كقاعدة بيانات :
+// EntityFrameworkCore.SqlServer
+// إذا كنت تستخدم SQL Server كقاعدة بيانات :
 
 Install-Package Microsoft.EntityFrameworkCore.SqlServer -Version 8.0.12
 
-EntityFrameworkCore.Tools
-لاستخدام الأوامر المتعلقة بـ EF Core مثل الترحيل (Migrations) :
+// EntityFrameworkCore.Tools
+// لاستخدام الأوامر المتعلقة بـ EF Core مثل الترحيل (Migrations) :
 
 Install-Package Microsoft.EntityFrameworkCore.Tools -Version 8.0.12
 
-Microsoft.EntityFrameworkCore.Proxies
- يتم استخدامها في Entity Framework Core لتفعيل خاصية Lazy Loading : 
+// Microsoft.EntityFrameworkCore.Proxies
+// يتم استخدامها في Entity Framework Core لتفعيل خاصية Lazy Loading : 
  
 Install-Package Microsoft.EntityFrameworkCore.Proxies -Version 8.0.12
 
 
+// You can use the following to install this pack easily: 
 
+// Go to any old project you will find a file with the extension : .csproj
+// Take the files between the brackets below: 
+// Put it in the main file :
 
+  <ItemGroup>
+    <PackageReference Include="Microsoft.EntityFrameworkCore" Version="8.0.12" />
+    <PackageReference Include="Microsoft.EntityFrameworkCore.Proxies" Version="8.0.12" />
+    <PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="8.0.12" />
+    <PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="8.0.12">
+    <PrivateAssets>all</PrivateAssets>
+    <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+    </PackageReference>
+  </ItemGroup>
+  
+// What changes is only the files inside.
+// In other words, according to the package you installed.
 
-
-
-
-
-
-
-- Use the following command to view the list of installed packages : 
+//Use the following command to view the list of installed packages : 
 Get-Package 
