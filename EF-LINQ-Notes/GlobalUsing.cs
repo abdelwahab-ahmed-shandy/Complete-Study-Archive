dc19c4a1438c59fc4b5dc38bf62cff2ffc1d26f7
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-02-16 21:45:22 +0200

    Create GlobalUsing.cs

---
+++ EF-LINQ-Notes/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using static System.Net.Mime.MediaTypeNames;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using System.ComponentModel.DataAnnotations;
