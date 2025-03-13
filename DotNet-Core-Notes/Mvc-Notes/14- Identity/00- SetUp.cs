a5804ab66e0193b579273fcbf417349aa4c014da
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-03-13 17:49:57 +0200

    Update and rename 01- Configure Identity.txt to 00- SetUp.cs

---
+++ DotNet-Core-Notes/Mvc-Notes/14- Identity/00- SetUp.cs
@@ -0,0 +1,18 @@
+// Install Packet Identity 
+
+/*To install the Microsoft.EntityFrameworkCore package version 8.0.12 in your .NET project, you can use the NuGet Package Manager Console in Visual Studio or the .NET CLI in your terminal.
+
+Option 1: Using NuGet Package Manager Console
+Open Visual Studio.
+
+Go to Tools > NuGet Package Manager > Package Manager Console.
+
+Run the following command:
+*/
+
+Install-Package Microsoft.EntityFrameworkCore -Version 8.0.12
+
+
+
+
+  
