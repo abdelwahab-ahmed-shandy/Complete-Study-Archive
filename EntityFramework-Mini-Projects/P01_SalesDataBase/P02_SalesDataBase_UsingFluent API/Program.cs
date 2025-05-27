518f49cc5ba51a9c259a71fa41aa06cce4e8d463
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-05-27 04:14:24 +0300

    Amendment EntityFramework Mini Projects

---
+++ EntityFramework-Mini-Projects/P01_SalesDataBase/P02_SalesDataBase_UsingFluent API/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P02_SalesDataBase_UsingFluent_API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You must first open Packege Manager Console  And Install Package :
+            /*
+            Install-Package Microsoft.EntityFrameworkCore -Version 8.0.12
+
+            Install-Package Microsoft.EntityFrameworkCore.SqlServer -Version 8.0.12
+
+            Install-Package Microsoft.EntityFrameworkCore.Tools -Version 8.0.12
+
+            */
+
+            // The rest you can do by following the existing recommendations.
+        }
+    }
+}
